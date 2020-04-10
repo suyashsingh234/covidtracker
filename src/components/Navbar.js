f207423da476b5f00c674d9ac9aab4831042da55
abhinavsri360 <abhinav.022000@gmail.com>
2020-04-10 23:48:14 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,32 @@
 import React from 'react'
-import { Form, FormControl, Button, NavDropdown, Nav } from 'react-bootstrap'
+import * as ReactBootstrap from 'react-bootstrap'
 
 function Navbar () {
   return (
     <div>
-      <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#link'>Link</Nav.Link>
-            <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.2'>Another action</NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href='#action/3.4'>Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form inline>
-            <FormControl type='text' placeholder='Search' className='mr-sm-2' />
-            <Button variant='outline-success'>Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
+      <ReactBootstrap.Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+        <ReactBootstrap.Navbar.Brand href='#home'>India's_Covi19_Tracker</ReactBootstrap.Navbar.Brand>
+        <ReactBootstrap.Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <ReactBootstrap.Navbar.Collapse id='responsive-navbar-nav'>
+          <ReactBootstrap.Nav className='mr-auto'>
+            <ReactBootstrap.Nav.Link href='#features'>State wise</ReactBootstrap.Nav.Link>
+            <ReactBootstrap.Nav.Link href='#pricing'>Precautions</ReactBootstrap.Nav.Link>
+            <ReactBootstrap.NavDropdown title='Dropdown' id='collasible-nav-dropdown'>
+              <ReactBootstrap.NavDropdown.Item href='#action/3.1'>option1</ReactBootstrap.NavDropdown.Item>
+              <ReactBootstrap.NavDropdown.Item href='#action/3.2'>option2</ReactBootstrap.NavDropdown.Item>
+              <ReactBootstrap.NavDropdown.Item href='#action/3.3'>option3</ReactBootstrap.NavDropdown.Item>
+              <ReactBootstrap.NavDropdown.Divider />
+              <ReactBootstrap.NavDropdown.Item href='#action/3.4'>Arogya setu</ReactBootstrap.NavDropdown.Item>
+            </ReactBootstrap.NavDropdown>
+          </ReactBootstrap.Nav>
+          <ReactBootstrap.Nav>
+            <ReactBootstrap.Nav.Link href='#deets'>See country wise</ReactBootstrap.Nav.Link>
+            <ReactBootstrap.Nav.Link eventKey={2} href='#memes'>
+            News
+            </ReactBootstrap.Nav.Link>
+          </ReactBootstrap.Nav>
+        </ReactBootstrap.Navbar.Collapse>
+      </ReactBootstrap.Navbar>
     </div>
   )
 }
