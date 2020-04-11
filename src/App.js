9e9b25fda66ff62dfe83f049f2458b6a64451c52
abhinavsri360 <abhinav.022000@gmail.com>
2020-04-12 00:41:56 +0530

    Map added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Map from './components/map'
 
 function App () {
   return (
     <div className='App'>
       <Navbar />
+      <Map />
     </div>
   )
 }
