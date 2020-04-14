efcdf5b08864ec8d66c5bb8fb998fac39149aa01
abhinavsri360 <abhinav.022000@gmail.com>
2020-04-15 01:10:19 +0530

    alignment

--- src/components/symptoms.js
+++ src/components/symptoms.js
@@ -16,33 +16,33 @@ function symptoms () {
             <br />
             <iframe title='common' src='https://giphy.com/embed/l4pSWx5JvWjnOHYXu' width='480' height='360' frameBorder='0' class='giphy-embed' allowFullScreen />
             <br />
-          </Col>      
+          </Col>
           <Col sm>
             <h3>Tiredness</h3>
             <br />
             <iframe title='tired' src='https://giphy.com/embed/LTYT5GTIiAMBa' width='480' height='360' frameBorder='0' class='giphy-embed' allowFullScreen />
             <br />
-          </Col>  
+          </Col>
           <Col sm>
             <h3>Dry Cough</h3>
             <br />
             <iframe title='dry' src='https://giphy.com/embed/L3Qzm7hzIZrXuN1OTu' width='418' height='360' frameBorder='0' class='giphy-embed' allowFullScreen />
             <br />
           </Col>
-      </Row>
-    </Container>
-    <br />
-    <br />
-    <Container fluid>
-          <h1>Other Symptoms include</h1>
-          <br />
+        </Row>
+      </Container>
+      <br />
+      <br />
+      <Container fluid>
+        <h1>Other Symptoms include</h1>
+        <br />
         <Row>
-          <Col sm>  
+          <Col sm>
             <h3>Shortness of Breath</h3>
             <br />
             <iframe title='breath' src='https://giphy.com/embed/YURXbwMSzBs5i' width='480' height='360' frameBorder='0' class='giphy-embed' allowFullScreen />
             <br />
-          </Col> 
+          </Col>
           <Col sm>
             <h3>Aches and Pains</h3>
             <br />
@@ -55,7 +55,7 @@ function symptoms () {
             <iframe title='throat' src='https://giphy.com/embed/l3V0dTVCyPDYEtxBK' width='480' height='360' frameBorder='0' class='giphy-embed' allowFullScreen />
             <br />
           </Col>
-      </Row>
+        </Row>
       </Container>
       <br />
       <br />
