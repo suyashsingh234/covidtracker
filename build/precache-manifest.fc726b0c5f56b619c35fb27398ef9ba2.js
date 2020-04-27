9f01ad6023c05fc1d20974edf3451b85dcbe8c14
abhinavsri360 <abhinav.022000@gmail.com>
2020-04-27 17:21:55 +0530

    build

---
+++ build/precache-manifest.fc726b0c5f56b619c35fb27398ef9ba2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5a12c31a69f53c577c123ecef467b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86fd551fbcc7748178ee",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3f1a5adc66d645154827",
+    "url": "/static/css/main.1817d960.chunk.css"
+  },
+  {
+    "revision": "86fd551fbcc7748178ee",
+    "url": "/static/js/2.8d593ba2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d593ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1a5adc66d645154827",
+    "url": "/static/js/main.c5025349.chunk.js"
+  },
+  {
+    "revision": "91981d32dc9e8ade310e",
+    "url": "/static/js/runtime-main.7ed7687a.js"
+  },
+  {
+    "revision": "2799f840a2ca1cf9c7a7c1caf1598e79",
+    "url": "/static/media/apple.2799f840.JPG"
+  },
+  {
+    "revision": "482dce8fe2ffc91fd287d8118f3488c2",
+    "url": "/static/media/github.482dce8f.png"
+  },
+  {
+    "revision": "ba5b4a0fe74e84d57396f3abf4b8e3fe",
+    "url": "/static/media/qr.ba5b4a0f.jpg"
+  }
+]);
